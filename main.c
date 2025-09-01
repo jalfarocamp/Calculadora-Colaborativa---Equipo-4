9a690519918fb3bd8c92b4a08222569f9df99b91
Gerard12121 <gerardorendon85@gmail.com>
2025-09-01 16:21:51 -0600

    Se implemento la interfaz y agrego la operación de resta

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include <stdlib.h>
 
+int resta(int a, int b){
+	return a-b;
+}
+
 void main (){
-	printf("hola mundo");
+	int op=0, a, b, r=0;
+	
+	printf("1 Suma\n");
+	printf("2 Resta\n");
+	printf("3 Multiplicacion\n");
+	printf("4 Salir\n");
+	
+	while(op!=4){
+	    printf("Ingresa la operacion a hacer\n");
+		scanf("%d", &op);
+		
+		if(op==4){
+			break;
+		}
+		
+		printf("\nIngresa el numero a:\n");
+		scanf("%d", &a);
+		printf("Ingresa el numero b:\n");
+		scanf("%d", &b);
+		
+		if(op==1){
+			
+		}else if(op==2){
+			r=resta(a,b);
+			printf("El resultado de la resta es: %d\n",r);
+		}else if(op==3){
+			
+		}
+	}
 }
